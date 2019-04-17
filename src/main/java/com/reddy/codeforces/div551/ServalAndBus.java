8019df86f524d7e8050232d3ae5bb7fee67173d8
Surendra Reddy <Surendra154@users.noreply.github.com>
2019-04-17 18:27:03 +0530

    Update ServalAndBus.java

--- src/main/java/com/reddy/codeforces/div551/ServalAndBus.java
+++ src/main/java/com/reddy/codeforces/div551/ServalAndBus.java
@@ -26,50 +26,25 @@ public class ServalAndBus {
         }
 
         for (int i = 0; i < n; i++) {
+            // compute when will the first bus in each route he can catch and compute the minimum
             int s = sdPairArr.get(i).getKey();
             int d = sdPairArr.get(i).getValue();
-            boolean isModified = false;
-            while( s < t) {
+
+            while(s < t) {
                 s += d;
-                isModified = true;
-                isTouched = true;
             }
-
-            if(isModified) {
-                s -= d; // since at final iteration goes over
-            }
-
             arr.add(s);
         }
 
-        if(arr.indexOf(t) > -1) {
-            System.out.println(arr.indexOf(t) + 1);
-        } else if(!isTouched) {
-            // get the min index
-            int minIndex = 0;
-            int min = arr.get(minIndex);
-            for (int i = 0; i < n; i++) {
-                if(arr.get(i) < min) {
-                    minIndex = i;
-                    min = arr.get(i);
-                }
+        int minIndex = 0;
+        int min = arr.get(minIndex);
+        for (int i = 0; i < n; i++) {
+            if(arr.get(i) < min) {
+                minIndex = i;
+                min = arr.get(i);
             }
-            System.out.println(minIndex+1);
-        } else {
-            // get the maximum index
-            int maxIndex = 0;
-            int max = arr.get(maxIndex);
-            for (int i = 0; i < n; i++) {
-                if(arr.get(i) > max) {
-                    maxIndex = i;
-                    max = arr.get(i);
-                }
-            }
-            System.out.println(maxIndex+1);
         }
-
-
-
+        System.out.println(minIndex+1);
 
     }
 
