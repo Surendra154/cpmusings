18a9a309f10ebff91f96083e5c00a61fab4edb70
Surendra Reddy <Surendra154@users.noreply.github.com>
2019-04-18 16:41:56 +0530

    Create UltraFastMathematician.java

---
+++ src/main/java/com/reddy/codeforces/UltraFastMathematician.java
@@ -0,0 +1,67 @@
+package com.reddy.codeforces;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class UltraFastMathematician {
+
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+
+        String b1 = fr.next();
+        String b2 = fr.next();
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < b1.length(); i++) {
+            if(b1.charAt(i) == b2.charAt(i)) {
+                ans.append("0");
+            } else {
+                ans.append("1");
+            }
+        }
+
+        System.out.println(ans.toString());
+    }
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return  Integer.parseInt(next());
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
